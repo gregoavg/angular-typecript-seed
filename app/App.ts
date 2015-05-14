aace156375c595cf38671a6b9ef29ba527ef4412
gregoavg <grigoris.avgitidis@gmail.com>
2015-05-14 22:10:21 +0300

    first commit

---
+++ app/App.ts
@@ -0,0 +1,12 @@
+/// <reference path="typings/tsd.d.ts" />
+
+angular.module("myApp", [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
